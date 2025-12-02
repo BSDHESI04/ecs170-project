aea78017bad8c98d8cec56ce8bdb08a22a225d53
Joshua Clark <joshaclark530@gmail.com>
2025-12-02 08:03:19 -0800

    Updated backend so it can send calls to Claude, gpt, and grok

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { sendPromptToGpt5Mini } = require('./gptClient');
+const { sendPromptToGrok4 } = require('./grokClient');
+const { sendPromptToClaude4Haiku } = require('./claudeClient');
+
 
 const app = express();
 const port = 5050; // Port 4000 doesn't work on my computer... idk why
@@ -12,16 +15,44 @@ app.use(express.json());
 app.post('/api/generate', async (req, res) => {
   try {
 
+    const { model } = req.body;
+    let answer = null
     const { prompt } = req.body;
 
-    const answer = await sendPromptToGpt5Mini(prompt);
+    if(model === 'gpt') {
+      answer = await sendPromptToGpt5Mini(prompt);
+      res.json({ result: answer });
+      return;
+    }
+    else if(model === 'grok') {
+    
+      answer = await sendPromptToGrok4(prompt);
+      res.json({ result: answer });
+      return; 
 
-    res.json({ result: answer });
+    }
+    else if(model === 'claude') {
+      answer = await sendPromptToClaude4Haiku(prompt);
+      res.json({ result: answer });
+      return;
+    }
+    else if(model === 'all') {
+      const claudeAnswer = await sendPromptToClaude4Haiku(prompt);
+      const gptAnswer = await sendPromptToGpt5Mini(prompt);
+      const grokAnswer = await sendPromptToGrok4(prompt);
+      res.json({ result: { claude: claudeAnswer, gpt: gptAnswer, grok: grokAnswer } });
+      return;
+    }
+    else {
+      res.status(400).json('Invalid model specified');
+      return;
+    }
 
   } 
   catch (error) {
 
     res.status(500).json('Error status 500 from /api/generate');
+    return;
 
   }
 });
