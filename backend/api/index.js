221dd947d4f26992a3a5cc1930e7a5e3d508cf7e
BSDHESI04 <bdhesi04@gmail.com>
2026-07-29 13:21:39 -0700

    made changes to index.js and vercel.json

---
+++ backend/api/index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { sendPromptToGpt5Mini } = require('../gptClient');
+const { sendPromptToGrok4 } = require('../grokClient');
+const { sendPromptToClaude4Haiku } = require('../claudeClient');
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/generate', async (req, res) => {
+  try {
+
+    const { model, prompt } = req.body;
+    let answer = '';
+
+    if(model === 'all') {
+      const claudeAnswer = await sendPromptToClaude4Haiku(prompt);
+      const gptAnswer = await sendPromptToGpt5Mini(prompt);
+      const grokAnswer = await sendPromptToGrok4(prompt);
+      return res.json({ result: { claude: claudeAnswer, gpt: gptAnswer, grok: grokAnswer } });
+      
+    }
+
+    if(model === 'gpt') {
+      answer = await sendPromptToGpt5Mini(prompt);
+    }
+    else if(model === 'grok') {
+      answer = await sendPromptToGrok4(prompt);
+    }
+    else if(model === 'claude') {
+      answer = await sendPromptToClaude4Haiku(prompt);
+    }
+    else {
+      res.status(400).json('Invalid model specified');
+      return;
+    }
+
+    res.json({ result: answer });
+    return;
+
+  } 
+  catch (error) {
+
+    res.status(500).json('Error status 500 from /api/generate');
+    return;
+
+  }
+});
+
+module.exports = app;
